6427df4782304409b6b54617e7e82e5c0cb9545b
Yuika <nguyenanhduc2938@gmail.com>
2024-08-27 23:02:51 +0700

    update react files

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -64,10 +64,10 @@ const Home = () => {
                       <BsInfoCircle className='text-2xl text-green-800' />
                     </Link>
                     <Link to={`/books/edit/${book._id}`}>
-                      <AiOutlineLink className='text-2xl text-yellow-600' />
+                      <AiOutlineEdit className='text-2xl text-yellow-600' />
                     </Link>
                     <Link to={`/books/delete/${book._id}`}>
-                      <BsInfoCircle className='text-2xl text-red-600' />
+                      <MdOutlineDelete className='text-2xl text-red-600' />
                     </Link>
                   </div>
                 </td>
